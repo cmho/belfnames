00b3d7ba12abe6a3cd0675914fe31037f07ddc99
Carly Ho <me@carlymho.com>
2013-07-18 17:26:28 -0500

    Add data for night elves, partial data for dwarves, add skeleton for goblins and gnomes

---
+++ dorfs.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+firstpart = ["Ale", "Alpha", "Amber", "Anvil", "Axe", "Armor", "Bale", "Black", "Barley", "Battle", "Bear", "Beer", "Blond", "Blue", "Blunder", "Blunt", "Boat", "Iron", "Bold", "Bone", "Boom", "Boulder", "Brad", "Brass", "Break", "Brew"]
+secondpart = ["tracker", "mage", "still", "beard", "mar", "hand", "fist", "hammer", "blade", "brew", "forge", "brawn", "snout", "anvil", "boot", "finger", "stone", "wall", "blast", "nose", "murder", "foot", "grip", "bellow", "drum", "toe", "warden", "helm", "bomb", "brow", "baster", "belly", "gold"]
+puts firstpart.sample + secondpart.sample
---
+++ goblins.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+firstpart = []
+secondpart = []
+puts firstpart.sample + secondpart.sample
