49bbfad571909ae501b0552d86d8160eb7e2acad
Carly Ho <me@carlymho.com>
2014-01-09 12:48:45 -0600

    Update readmie

--- goblins.rb
+++ goblins.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 
-firstpart = []
-secondpart = []
+firstpart = ["Bang", "Barrow", "Bazzel", "Big", "Bilge", "Black", "Blast", "Bling", "Blunt", "Bluster", "Bog", "Bolt", "Bomb", "Bone", "Boom", "Boot", "Bottle", "Bracket", "Brain", "Bramble", "Brass", "Brew", "Brim", "Bucks", "Cable", "Calla", "Chop", "Cloud", "Clutch", "Coif", "Coil", "Coin". "Copper", "Crackle", "Crank", "Cross", "Cryo", "Crystal", "Curtain", "Cute", "Cutler", "Cut", "Damp"]
+secondpart = ["wrench", "swizzle", "flange", "blast", "whizzle", "fuse", "bolt", "hammer", "noggin", "pipe", "torch", "bang", "buckle", "strike", "pipe", "brew", "bucket", "cutter", "knuckle", "shine", "strider", "saw", "boom", "getter", "ride", "son", "scuff", "rocket", "swing", "boiler", "brush", "bearing", "brush", "knob", "tacks", "tumblr", "right", "tap", "buzzle", "quick", "clamp", "gutz", "limb", "kicker", "pop", "curl", "ee", "spanner", "span", "catcher", "claw", "clip", "grab", "nickels", "nut", "pinch", "spring", "plug", "fizz", "chain", "wheel", "check", "wire", "flight", "gear", "fire", "sazz", "flix", "wick"]
 puts firstpart.sample + secondpart.sample
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
-# Blood Elf Name Generator
+# World of Warcraft Name Generator
 
-Exactly what it says on the tin.
+Generates surnames in the format of [Noun][noun], [Noun][verber], [Adjective][noun], etc., drawing from the canonical names of World of Warcraft NPCs and lore characters.  Used to be only blood elves—I should probably rename the repo.
+
+Also, apparently Draenei just don't have surnames? I found like one with a surname on the wiki.  What's up with that? (They're kind of sparse for trolls, too.)
+
+If I get bored I might see if I can parse all the first names into some kind of magic generating thing, too.  It'd make naming new characters way easier, for one thing.
